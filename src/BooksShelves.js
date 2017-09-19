92b703c7b024569b0598eab2dc79ff6d5a75752a
Renan Lopes <renanlopescoder@gmail.com>
2017-09-18 22:02:14 -0300

    Creating BookComponent

--- src/BooksShelves.js
+++ src/BooksShelves.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Book from './BookComponent'
 
 class BooksShelves extends Component {
 
@@ -39,52 +40,7 @@ class BooksShelves extends Component {
               <ol className="books-grid">
               {
                 currentlyReading.map((book) => (
-                  <li key={book.id}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
-                      <div className="book-shelf-changer">
-                        <select onChange={
-                              (event) => {
-                                this.props.updateBook(book, event.target.value)
-                              }
-                            }>
-                          <option value="none" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                          <option value="rate">Rate</option>
-                        </select>
-                      </div>
-                      <div className="book-rate">
-                      <select onChange={
-                              (event) => {
-                                this.props.rateBook(book,book.shelf,event.target.value)
-                              }
-                            }>
-                          <option value="none" disabled>Rate Book</option>
-                          <option value="[]">No Rate</option>
-                          <option value="[1]">1 Star</option>
-                          <option value="[1,2]">2 Star</option>
-                          <option value="[1,2,3]">3 Star</option>
-                          <option value="[1,2,3,4]">4 Star</option>
-                          <option value="[1,2,3,4,5]">5 Star</option>
-                        </select>
-                      </div>
-                    </div>
-                    {
-                      book.rate.map(
-                        (id) => (
-                            <div key={id} className="book-starFull"></div>
-                        )
-                      )
-                    }
-                    <br />
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors}</div>
-                  </div>
-                </li>
+                  <Book key={book.id} book={book} updateBook={this.props.updateBook} rateBook={this.props.rateBook}/> 
                 )) 
               }
               </ol>
@@ -96,51 +52,7 @@ class BooksShelves extends Component {
               <ol className="books-grid">
               {
                 wantToRead.map((book) => (
-                  <li key={book.id}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
-                      <div className="book-shelf-changer">
-                        <select onChange={
-                              (event) => {
-                                this.props.updateBook(book, event.target.value)
-                              }
-                            }>
-                          <option value="none" disabled>Move to...</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                      <div className="book-rate">
-                      <select onChange={
-                              (event) => {
-                                this.props.rateBook(book,book.shelf,event.target.value)
-                              }
-                            }>
-                          <option value="none" disabled>Rate Book</option>
-                          <option value="[]">No Rate</option>
-                          <option value="[1]">1 Star</option>
-                          <option value="[1,2]">2 Star</option>
-                          <option value="[1,2,3]">3 Star</option>
-                          <option value="[1,2,3,4]">4 Star</option>
-                          <option value="[1,2,3,4,5]">5 Star</option>
-                        </select>
-                      </div>
-                    </div>
-                    {
-                      book.rate.map(
-                        (id) => (
-                            <div key={id} className="book-starFull"></div>
-                        )
-                      )
-                    }
-                    <br />
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors}</div>
-                  </div>
-                </li>
+                  <Book key={book.id} book={book} updateBook={this.props.updateBook} rateBook={this.props.rateBook}/>
                 )) 
               }
               </ol>
@@ -152,51 +64,7 @@ class BooksShelves extends Component {
               <ol className="books-grid">
               {
                 read.map((book) => (
-                  <li key={book.id}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
-                      <div className="book-shelf-changer">
-                        <select name="select" onChange={
-                              (event) => {
-                                this.props.updateBook(book, event.target.value)
-                              }
-                            }>
-                          <option value="none" disabled>Move to...</option>
-                          <option value="read">Read</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                      <div className="book-rate">
-                      <select onChange={
-                              (event) => {
-                                this.props.rateBook(book,book.shelf,event.target.value)
-                              }
-                            }>
-                          <option value="none" disabled>Rate Book</option>
-                          <option value="[]">No Rate</option>
-                          <option value="[1]">1 Star</option>
-                          <option value="[1,2]">2 Star</option>
-                          <option value="[1,2,3]">3 Star</option>
-                          <option value="[1,2,3,4]">4 Star</option>
-                          <option value="[1,2,3,4,5]">5 Star</option>
-                        </select>
-                      </div>
-                    </div>
-                    {
-                      book.rate.map(
-                        (id) => (
-                            <div key={id} className="book-starFull"></div>
-                        )
-                      )
-                    }
-                    <br />
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors}</div>
-                  </div>
-                </li>
+                  <Book key={book.id} book={book} updateBook={this.props.updateBook} rateBook={this.props.rateBook} />
                 )) 
               }
               </ol>
